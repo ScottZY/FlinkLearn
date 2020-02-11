2558e80aa77977c6952bcebfc27a0fdf52e6145c
z_y <yongzhang182@outlook.com>
2020-02-11 12:29:52 +0800

    20200211第二次提交

--- src/main/java/com/imooc/flink/Course04/DataSetTransformationJ.java
+++ src/main/java/com/imooc/flink/Course04/DataSetTransformationJ.java
@@ -1,10 +1,14 @@
 package com.imooc.flink.Course04;
 
+import com.imooc.flink.scala.course04.DBUtils;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.MapPartitionFunction;
+import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
-
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +20,54 @@ public class DataSetTransformationJ {
 //        构建执行环境
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 //        mapFunction(env);
-        filterFunction(env);
+//        filterFunction(env);
+//        mapPartitionFunction(env);
+        firstFunction(env);
+    }
+
+    /**
+     * first函数
+     * @param env
+     * @throws Exception
+     */
+    public static void firstFunction(ExecutionEnvironment env) throws Exception{
+        List<Tuple2<Integer, String>> info = new ArrayList<Tuple2<Integer, String>>();
+        info.add(new Tuple2<Integer, String>(1, "hadoop"));
+        info.add(new Tuple2<Integer, String>(1, "Spark"));
+        info.add(new Tuple2<Integer, String>(1, "Flink"));
+        info.add(new Tuple2<Integer, String>(2, "Java"));
+        info.add(new Tuple2<Integer, String>(2, "Ruby"));
+        info.add(new Tuple2<Integer, String>(3, "Linux"));
+        info.add(new Tuple2<Integer, String>(3, "Unix"));
+        final DataSource<Tuple2<Integer, String>> data = env.fromCollection(info);
+        System.out.println("~~~~~~~~~~~~我是分割线~~~~~~~~~~~~~");
+        data.first(3).print();
+        System.out.println("~~~~~~~~~~~~我是分割线~~~~~~~~~~~~~");
+        data.groupBy(0).first(2).print();
+        System.out.println("~~~~~~~~~~~~我是分割线~~~~~~~~~~~~~");
+        data.groupBy(0).sortGroup(1, Order.DESCENDING).first(2).print();
+    }
+
+
+    /**
+     * mapPartition function
+     * @param env
+     */
+    public static void mapPartitionFunction(ExecutionEnvironment env) throws Exception {
+        List<String> list = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++){
+            list.add("Students: "+i);
+        }
+        DataSource<String> data = env.fromCollection(list).setParallelism(6);
+//        使用 mapPartition 创建41次
+        data.mapPartition(new MapPartitionFunction<String, String>() {
+            @Override
+            public void mapPartition(Iterable<String> values, Collector<String> out) throws Exception {
+                String conn = DBUtils.getConnection();
+                System.out.println(conn + "...");
+                DBUtils.returnFunction(conn);
+            }
+        }).print();
     }
 
     /**
