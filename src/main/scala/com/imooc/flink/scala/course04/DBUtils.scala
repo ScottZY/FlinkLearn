753d7fd3d0da54a0f917f89ac75c03b075c6c7ac
z_y <yongzhang182@outlook.com>
2020-02-11 08:36:52 +0800

    模拟数据库连接测试mapPartition函数

---
+++ src/main/scala/com/imooc/flink/scala/course04/DBUtils.scala
@@ -0,0 +1,13 @@
+package com.imooc.flink.scala.course04
+
+import scala.util.Random
+
+object DBUtils {
+  def getConnection(): Unit ={
+    new Random().nextInt(10)+"1"
+  }
+  def returnFunction(): Unit ={
+
+  }
+
+}
